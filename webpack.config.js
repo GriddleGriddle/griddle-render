952e13dd12139ca74fc2ae03d2407b7f2ffeb948
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2015-10-10 14:43:41 -0400

    Remove redux connector

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,24 @@
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'source-map',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './index'
-  ],
+  devtool: 'eval',
+  entry: './index',
   output: {
     path: __dirname + '/build/',
-    filename: 'griddle.js',
-    publicPath: '/build/'
+    filename: 'griddle-render.js',
+    publicPath: '/build/',
+    libraryTarget: 'commonjs2'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
   ],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders: [
-      { test: /\.jsx?$/, loaders: ['react-hot', 'babel?{"plugins":["babel-plugin-object-assign"]}'], exclude: /node_modules/ }
-    ]
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      exclude: /node_modules/,
+    } ]
   }
 };
