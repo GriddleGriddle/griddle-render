eba26e91f7a76dfe8f063c7b078b9568ee4f4b52
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2015-11-08 16:15:56 -0500

    Add styles from original griddle

--- src/defaultStyles.js
+++ src/defaultStyles.js
@@ -1,4 +1,49 @@
-//export inlineStyles
+export const inlineStyles = {
+  settingsButton: { 
+    background: 'none',
+    border: 'none',
+    padding: 0,
+    margin: 0,
+    fontSize: 14
+  },
+
+  filter: {
+    'float': 'left',
+    width: '50%',
+    textAlign: 'left',
+    color: '#222',
+    minHeight: '1px'
+  },
+
+  columnTitle: {
+    backgroundColor: '#EDEDEF',
+    border: '0',
+    borderBottom: '1px solid #DDD',
+    color: '#222',
+    padding: '5px',
+    cursor: 'pointer'
+  },
+
+  column: {
+    margin: '0',
+    padding: that.props.paddingHeight + 'px 5px ' + that.props.paddingHeight + 'px 5px',
+    height: that.props.rowHeight? this.props.rowHeight - that.props.paddingHeight * 2 + 'px' : null,
+    backgroundColor: '#FFF',
+    borderTopColor: '#DDD',
+    color: '#222'
+  },
+
+  paging: {
+    'padding' : '0',
+    backgroundColor: '#EDEDED',
+    border: '0',
+    color: '#222'
+  },
+
+  tableFixed: {
+    tableLayout: 'fixed'
+  }
+}
 
 export const classNames = {
   column: 'griddle-column',
@@ -16,7 +61,7 @@ export const classNames = {
 }
 
 export const icons = {
-  //TODO: these need to get moved to something that adds these on as part of the subgrid "plugin"
+  //TODO: these need to get moved to something that adds these on as part of the subgrid 'plugin'
   parentRowCollapsed: '▶',
   parentRowExpanded: '▼',
   sortDescending: '▼',
