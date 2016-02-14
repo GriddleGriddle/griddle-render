00a7c8f81151c973db3559b767f46595e1ca8eae
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2016-02-14 16:45:27 -0500

    Switch to function component

--- src/pagination.js
+++ src/pagination.js
@@ -1,38 +1,47 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { getStyleProperties } from './utils/styleHelper';
 
-class Pagination extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+import { compose, getContext, mapProps } from 'recompose';
 
-    this._handleChange = this._handleChange.bind(this);
+function getPageNumberDropdown(props) {
+  if(!props.pageProperties || !props.pageProperties.maxPage ) { return; }
+
+  //Make this nicer
+  var options = [];
+
+  for(var i = 1; i <= props.pageProperties.maxPage; i++) {
+    options.push(<option value={i} key={i}>{i}</option>)
   }
 
-  render() {
-    const {style, className } = getStyleProperties(this.props, 'pagination');
-
-    return (<div className={className} style={style}>
-      {this.props.hasPrevious ? <button onClick={this.props.events.getPreviousPage}>Previous</button> : null }
-      {this._getSelect()}
-      {this.props.hasNext ? <button onClick={this.props.events.getNextPage}>Next</button> : null }
-    </div>);
-  }
-
-  _handleChange(e) {
-    this.props.events.getPage(parseInt(e.target.value));
-  }
-
-  _getSelect() {
-    if( !this.props.pageProperties || !this.props.pageProperties.maxPage ) { return; }
-    //Make this nicer
-    var options = [];
-
-    for(var i = 1; i <= this.props.pageProperties.maxPage; i++) {
-      options.push(<option value={i} key={i}>{i}</option>)
-    }
-
-    return <select onChange={this._handleChange} value={this.props.pageProperties.currentPage}>{options}</select>
-  }
+  return (
+    <select onChange={props.handleChange}
+      value={props.pageProperties.currentPage}>
+      {options}
+    </select>
+  );
 }
+
+const Pagination = compose(
+  getContext({ utils: PropTypes.object }),
+
+  mapProps(props => ({
+    styleProperties: props.utils.getStyleProperties(props, 'pagination'),
+    handleChange: (e) => props.events.getPage(parseInt(e.target.value)),
+    ...props
+  })),
+
+  mapProps(props => ({
+    pageNumberDropdown: getPageNumberDropdown(props),
+    style: props.styleProperties.style,
+    className: props.styleProperties.className,
+    ...props
+  }))
+)(({ style, className, pageNumberDropdown, handleChange, hasNext, hasPrevious, events }) => (
+  <div className={className} style={style}>
+    { hasPrevious ? <button onClick={events.getPreviousPage}>Previous</button> : null }
+    {pageNumberDropdown}
+    { hasNext ? <button onClick={events.getNextPage}>Next</button> : null }
+  </div>
+))
 
 export default Pagination;
