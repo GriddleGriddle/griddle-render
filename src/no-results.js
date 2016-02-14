25fe9c0c362af70889d486462b8952315cd1c7c0
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2016-02-14 16:27:25 -0500

    Switch no-results to function component

--- src/no-results.js
+++ src/no-results.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import {getStyleProperties}  from './utils/styleHelper';
 
-class NoResults extends React.Component {
-  render() {
-    const { style, className } = getStyleProperties(this.props, 'noResults');
+import { compose, getContext, mapProps } from 'recompose';
 
-    return (
-      <div style={style} className={className}>
-        <h4>No results found.</h4>
-      </div>
-    )
-  }
-}
+const NoResults = compose(
+  getContext({ utils: PropTypes.object }),
+
+  mapProps(props => ({
+    styleProperties: props.utils.getStyleProperties(props, 'noResults'),
+    ...props
+  })),
+
+  mapProps(props => ({
+    style: props.styleProperties.style,
+    className: props.styleProperties.className
+  }))
+)(({ style, className }) => (
+  <div style={style} className={className}>
+    <h4>No results found.</h4>
+  </div>
+))
 
 export default NoResults;
