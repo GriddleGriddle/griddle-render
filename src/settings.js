8d80179e5deb27980e5be73eae748d498ae00597
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2016-02-14 21:21:19 -0500

    Switch some of the local settings components to functions

--- src/settings.js
+++ src/settings.js
@@ -1,50 +1,39 @@
 import React from 'react';
 import { getStyleProperties } from './utils/styleHelper';
 
-class CheckItem extends React.Component {
-  render() {
-    return (
-      <label onClick={this._handleClick}>
-        <input type="checkbox" checked={this.props.checked} name={this.props.name} />
-        {this.props.text}
-      </label>);
-  }
+import { compose, getContext, mapProps, shouldUpdate } from 'recompose';
 
-  _handleClick = () => {
-    this.props.toggleColumn(this.props.name);
-  }
-}
+const CheckItem = mapProps(props => ({
+  shouldUpdate: (() => false),
 
-CheckItem.propTypes = {
-  toggleColumn: React.PropTypes.func.isRequired,
-  name: React.PropTypes.string.isRequired,
-  checked: React.PropTypes.bool,
-  value: React.PropTypes.oneOfType([
-    React.PropTypes.string,
-    React.PropTypes.Number
-  ]),
-  text: React.PropTypes.string
-};
+  handleClick: () => props.toggleColumn(props.name),
+  ...props
+}))(({ name, checked, value, text, handleClick}) => (
+  <label onClick={handleClick}>
+    <input type="checkbox" checked={checked} name={name} />
+    {text}
+  </label>
+))
 
-class PageSize extends React.Component {
-  render() {
-    return (
-      <select name="pageSize" onChange={this._handleChange}>
-        {this.props.sizes.map(size => <option value={size}>{size}</option>)}
-      </select>
-    );
-  }
+const PageSize = compose(
+  mapProps(props => ({
+    defaultSizes: [5, 10, 20, 30, 50, 100],
+    handleChange: (e) => { props.setPageSize(parseInt(e.target.value)) },
+    ...props
+  })),
+  mapProps(props => ({
+    options: props.defaultSizes.map(size => <option value={size}>{size}</option>),
+    ...props
+  }))
+)(({ handleChange, options }) => (
+  <select name="pageSize" onChange={handleChange}>
+    {options}
+  </select>
+));
 
-  _handleChange = (e) => {
-    this.props.setPageSize(parseInt(e.target.value));
-  }
-}
-
-PageSize.defaultProps = {
-  sizes: [5, 10, 20, 30, 50, 100]
-}
 class Settings extends React.Component {
   render() {
+
     const keys = Object.keys(this.props.renderProperties.columnProperties);
     const { style, className } = getStyleProperties(this.props, 'settings');
 
